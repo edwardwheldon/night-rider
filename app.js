a7498739a7559008777c11c7962884e3a83e71cd
TJSteel <jaysteel78@msn.com>
2022-04-17 22:01:23 +0100

    split led reduction into a function

--- app.js
+++ app.js
@@ -1,39 +1,7 @@
-// const leds = document.getElementsByClassName("led-glow");
-
-// console.log(document.getElementsByClassName("led-glow").length)
-
-// let l = 0;
-// let forward = true;
-// function updateLed() {
-
-//     if (forward) {
-//         leds[i].style.opacity = "1";
-//         l++;
-//         if (l > 5) {
-//             l = 4;
-//             forward = false;
-//         }
-
-//     } else {
-//         l--;
-//         if (l < 0) {
-//             l = 1;
-//             forward = true;
-//         }
-//     }
-//     leds[i].style.opacity = "1"; // on
-//     // leds[l].classList.remove('animate');
-//     // leds[l].classList.add('animate');
-//   setTimeout(updateLed, 150)
-// }
-// updateLed();
 const speed = 150;
 const leds = document.getElementsByClassName("led-glow");
 
-let direction = 1;
-let l = 1;
-function updateLeds() {
-  clearTimeout(ledTimeout);
+function reduceLedBrightness() {
   for (const led of leds) {
     let opacity = parseFloat(led.style.opacity);
     opacity -= 0.3;
@@ -42,6 +10,13 @@ function updateLeds() {
     }
     led.style.opacity = opacity;
   }
+}
+
+let direction = 1;
+let l = 1;
+function updateLeds() {
+  clearTimeout(ledTimeout);
+  reduceLedBrightness();
   l += direction;
   if (l > 5 || l < 0) {
     l -= direction * 2;
