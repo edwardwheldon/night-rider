664066ee4c94390faba47d6d6d33fa731b46e104
edwardwheldon <edward.wheldon@googlemail.com>
2022-04-17 21:38:13 +0100

    lights

--- app.js
+++ app.js
@@ -1,30 +1,65 @@
-const leds = document.getElementsByClassName("on");
+// const leds = document.getElementsByClassName("led-glow");
 
-console.log(document.getElementsByClassName("on").length)
+// console.log(document.getElementsByClassName("led-glow").length)
+
+// let l = 0;
+// let forward = true;
+// function updateLed() {
+    
+//     if (forward) {
+//         leds[i].style.opacity = "1"; 
+//         l++;
+//         if (l > 5) {
+//             l = 4;
+//             forward = false;
+//         }
+
+//     } else {
+//         l--;
+//         if (l < 0) {
+//             l = 1;
+//             forward = true;
+//         }
+//     }
+//     leds[i].style.opacity = "1"; // on
+//     // leds[l].classList.remove('animate');
+//     // leds[l].classList.add('animate');
+//   setTimeout(updateLed, 150)
+// }
+// updateLed();
+const speed = 150;
+const leds = document.getElementsByClassName("led-glow");
 
 
-let l = 0;
-let forward = true;
-function updateLed() {
-    if (forward) {
-        l++;
-        if (l > 5) {
-            l = 4;
-            forward = false;
-        }
-
-    } else {
-        l--;
-        if (l < 0) {
-            l = 1;
-            forward = true;
-        }
+let forward = false;
+let l = 1;
+function updateLeds() {
+  clearTimeout(ledTimeout);
+  for (const led of leds) {
+    let opacity = parseFloat(led.style.opacity);
+    opacity -= 0.3;
+    if (opacity < 0) {
+      opacity = 0;
     }
-    leds[l].classList.remove('animate');
-    leds[l].classList.add('animate');
-  setTimeout(updateLed, 150)
+    led.style.opacity = opacity;
+  }
+  if (forward) {
+    l++;
+    if (l > 5) {
+      l = 4;
+      forward = false;
+    }
+  } else {
+    l--;
+    if (l < 0) {
+      l = 1;
+      forward = true;
+    }
+  }
+  leds[l].style.opacity = 1;
+  ledTimeout = setTimeout(updateLeds, speed);
 }
-updateLed();
+let ledTimeout = setTimeout(updateLeds, speed);
 
 
 
